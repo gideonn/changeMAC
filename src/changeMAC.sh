0dd020bf322c82338ba83cc67e75375d8f066374
Abhishek Srivastava <srivastava.abhishek.sa@gmail.com>
2017-11-16 19:57:47 -0500

    Moved the file to src, added comments

--- src/changeMAC.sh
+++ src/changeMAC.sh
@@ -6,12 +6,16 @@
 #                 #
 ###################
 
+#Get the name of the wifi interface
 interface_name=`ip link | grep -v lo | grep "mtu" | cut -d ":" -f2 | tr -d ' '`
+
+#Generate the new MAC address
 new_mac=`tr -dc A-F0-9 < /dev/urandom | head -c 10 | sed -r 's/(..)/\1:/g;s/:$//;s/^/02:/'`
+
+#Get the current MAC address
 curr_mac=`ip link | grep -v lo | grep "link" | sed -e 's/^[[:space:]]*//g' | cut -d " " -f2`
 
 echo "Current mac address is: ${curr_mac}"
-
 echo "Shutting down interface ${interface_name}"
 
 ifconfig ${interface_name} down
@@ -20,13 +24,14 @@ ifconfig ${interface_name} up
 
 echo "Resetting network manager to fill routing table, please wait..."
 sudo /etc/init.d/network-manager restart
-
 sleep 10
 
 upd_mac=`ip link | grep -v lo | grep "link" | sed -e 's/^[[:space:]]*//g' | cut -d " " -f2`
+
+#Check if the update was successful
 if [[ "${upd_mac}" == "${curr_mac}" ]];
 then
-	echo "Failed to update the mac address, did you use sudo ?"
+	echo "Failed to update the mac address, something wrong with the config? "
 else
 	echo ${upd_mac} >> mac_addr_history.txt
 	echo "Updated mac address, now current mac address is: ${upd_mac}"
